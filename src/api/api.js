c1123a79a2c1e3d42e402e268f38981be3d5500e
fajar m <fajarm@orderonline.id>
2024-01-27 00:57:52 +0700

    change api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://otakudesu-unofficial-api.rzkfyn.xyz/api/v1/', // Ganti dengan URL base API server Anda
+  baseURL: 'https://otakudesu-unofficial-api.rzkfyn.xyz/v1/', // Ganti dengan URL base API server Anda
   timeout: 60000, // Waktu timeout untuk permintaan
 });
 
